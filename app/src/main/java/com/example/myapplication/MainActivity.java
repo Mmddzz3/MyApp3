ebe3c0266725d9553de50b92cc3270e58aaa71f3
Mmddzz3 <madhura.pharande@somaiya.edu>
2019-04-23 16:12:40 +0530

    Changed background colour

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    public void nextAct(View v) {
+        Intent i = new Intent(this, Main2Activity.class);
+        startActivity(i);
+
+    }
+
+
+
+}
